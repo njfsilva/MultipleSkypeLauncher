9e18ac72d037dfe4ff88ac969e1364f44f7e1755
Nelson Silva <nelson.jf.silva@gmail.com>
2015-05-21 14:20:02 +0100

    Initial Commit
    Application usable by itself, Accounts.xml needs to be created on
    executable directory

---
+++ SkypeLauncherConsole/IOHelper.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SkypeLauncherConsole
+{
+    public static class IOHelper
+    {
+        public static List<Account> LoadAccounts(string accountFile)
+        {
+            if (!File.Exists(accountFile))
+            {
+                File.Create(accountFile);
+            }
+
+            try
+            {
+                using (var stream = new FileStream(accountFile, FileMode.Open))
+                {
+                    var xml = new XmlSerializer(typeof (List<Account>));
+                    return (List<Account>) (xml.Deserialize(stream));
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static bool SaveAccounts(List<Account> accounts, string accountsFile)
+        {
+            try
+            {
+                using (var stream = new FileStream(accountsFile, FileMode.OpenOrCreate))
+                {
+                    var xml = new XmlSerializer(typeof (List<Account>));
+                    xml.Serialize(stream, accounts);
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+    } 
+}
---
+++ SkypeLauncherConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+
+namespace SkypeLauncherConsole
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var accounts = IOHelper.LoadAccounts(StaticStrings.AccountsFileName);
+
+            if (accounts == null) return;
+
+            var skypeLocation = ConfigurationManager.AppSettings["SkypeLocation"];
+
+            if (!string.IsNullOrEmpty(skypeLocation))
+            {
+                accounts.ForEach(account =>
+                {
+                    var username = account.Username.Trim();
+                    var password = account.Password.Trim();
+
+                    var start = new ProcessStartInfo
+                    {
+                        Arguments = string.Format("/Secondary /nosplash /minimized /username:{0} /password:{1}", username, password),
+                        FileName = skypeLocation.Trim()
+                    };
+
+                    using (var proc = Process.Start(start))
+                    {
+                        if (proc == null) return;
+                        proc.Close();
+                    }
+                });
+            }
+        }
+         
+    }
+}
